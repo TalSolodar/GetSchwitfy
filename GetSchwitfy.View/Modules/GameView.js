9ff44148507e6eeba2afb7b5e6eb25c497197163
TalSolodar <talsolodar1@gmail.com>
2022-07-04 20:43:29 +0300

    GameView: contain all the view features

--- GetSchwitfy.View/Modules/GameView.js
+++ GetSchwitfy.View/Modules/GameView.js
@@ -1,11 +1,17 @@
 class GameView{
 
-    constructor(boardPrinter, buttonsEventHandler, buttonUpdater, winnerNotifier, inputBoardSize){
+    constructor(boardPrinter,
+                buttonsEventHandler,
+                buttonUpdater,
+                winnerNotifier,
+                inputBoardSize,
+                inputPlayerName){
         this.buttonsEventHandler = buttonsEventHandler;
         this.boardPrinter = boardPrinter;
         this.buttonUpdater = buttonUpdater;
         this.winnerNotifier = winnerNotifier;
         this.inputBoardSize = inputBoardSize;
+        this.inputPlayerName = inputPlayerName;
     }
 
 }
