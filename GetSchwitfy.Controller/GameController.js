8f636b8d2bb74a13ba44caa7c9c20f0a721b3b43
TalSolodar <talsolodar1@gmail.com>
2022-07-04 20:37:37 +0300

    GameController: get games model and add games to it

--- GetSchwitfy.Controller/GameController.js
+++ GetSchwitfy.Controller/GameController.js
@@ -1,15 +1,18 @@
 class GameController{
     
-    constructor(gameSquaresInitializer, gameView, squareMovementHandler, victoryChecker){
+    constructor(gameSquaresInitializer, gameView, squareMovementHandler, victoryChecker, gamesModel){
         this.gameView = gameView;
         this.squareMovementHandler = squareMovementHandler;
         this.gameSquaresInitializer = gameSquaresInitializer;
         this.victoryChecker = victoryChecker;
+        this.gamesModel = gamesModel;
         this.CreateGameBoard();
     }
 
     CreateGameBoard(){
+        let playerName = this.gameView.inputPlayerName.GetName();
         let boardSize = this.gameView.inputBoardSize.GetBoardSize();
+        this.AddGameToModel(playerName, boardSize);
         this.gameBoard = this.gameSquaresInitializer.InitializeGameBoard(boardSize, boardSize);
         this.squareMovementHandler.SetNumberLines(boardSize);
         this.squareMovementHandler.SetNumberSquaresInLine(boardSize);
@@ -19,14 +22,19 @@ class GameController{
     }
 
     HandleButtonEvent(numberButton, buttons){
-        console.log("here");
         this.squareMovementHandler.gameBoard = this.gameBoard.squares;
         this.squareMovementHandler.TryMoveSquare(numberButton);
         this.gameView.buttonUpdater.UpdateButtonsValues(squareMovementHandler.gameBoard, buttons);
         if(this.victoryChecker.IsBoardSolved(this.gameBoard.squares)){
             this.gameView.winnerNotifier.NotifyVictory();
             this.gameView.boardPrinter.buttons = [];
+            this.gamesModel.EndGame();
             this.CreateGameBoard();
         }
+    }
+
+    AddGameToModel(playerName, boardSize){
+        this.gamesModel.AddGame(playerName, boardSize);
+        console.log(this.gamesModel.games);
     }
 }
