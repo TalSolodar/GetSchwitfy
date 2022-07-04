f4e5c6af0417cf085c0071287ed8bf8a8957c4d9
TalSolodar <talsolodar1@gmail.com>
2022-07-04 14:49:46 +0300

    GameSquareInitializer: initalize the boarder

--- GetSchwitfy.Controller/GameSquaresInitializer.js
+++ GetSchwitfy.Controller/GameSquaresInitializer.js
@@ -3,14 +3,15 @@ const NumberSquaresInLine = 2;
 
 class GameSquaresInitializer{
 
-    constructor(){
+    constructor(gameSolvableChecker){
         this.squaresNumbers = [];
+        this.gameSolvableChecker = gameSolvableChecker;
     }
 
     InitializeGameBoard(){
         let gameBoard = this.CreateBoard();
     
-        while(!this.IsGameSolvableׂׂ(gameBoard)){
+        while(!this.gameSolvableChecker.IsGameSolvableׂׂ(gameBoard)){
             gameBoard = this.CreateBoard();
         }
 
@@ -50,65 +51,5 @@ class GameSquaresInitializer{
         let square = new Square(squareRandomNumber);
         
         return square;
-    }
-
-    IsGameSolvableׂׂ(gameBoard){
-        let gameBoardSquares = gameBoard.squares.flat(); 
-        if(NumberLines % 2 == 0){
-            return this.IsGameWithEvenNumberLinesSolvable(gameBoardSquares);
-        }
-        else{
-            return this.IsGameWithOddNumberLinesSolvable(gameBoardSquares);
-        }       
-    }
-
-    IsGameWithEvenNumberLinesSolvable(gameBoardSquares){
-        let indexEmptySquare = gameBoardSquares.indexOf(NumberLines*NumberSquaresInLine);
-        let squares = gameBoardSquares.slice(indexEmptySquare, gameBoardSquares.length)
-        
-        if(squares.length == 0){
-            return true;
-        }
-
-        let opposites = this.CreateCounterOpposite(gameBoardSquares);
-        let isSolvable = this.IsCounterOppositeEven(opposites);
-
-        return isSolvable;
-    }
-
-    IsGameWithOddNumberLinesSolvable(gameBoardSquares){
-        let opposites = this.CreateCounterOpposite(gameBoardSquares);
-        let isSolvable = this.IsCounterOppositeEven(opposites);
-
-        return isSolvable;
-    }
-
-    CreateCounterOpposite(gameBoardSquares){
-        let opposites = [];
-
-        for(let i = 0 ; i <gameBoardSquares.length; i++){
-            opposites[i] = 0;
-        }
-
-        for(let i = 0 ; i < gameBoardSquares.length -1 ; i++){
-            for(let j = i+1 ; j < gameBoardSquares.length;  j++){
-                if(gameBoardSquares[i].number > gameBoardSquares[j].number){
-                    opposites[i] += 1;
-                }
-            }
-        }
-        return opposites;
-    }
-
-    IsCounterOppositeEven(opposites){
-        let counterOpposites = 0 ;
-
-        opposites.forEach((opposite) => {
-            counterOpposites += opposite;
-        }); 
-
-        let IsCountOppositeEven = counterOpposites % 2 == 0 ;
-
-        return IsCountOppositeEven;
     }
 }
