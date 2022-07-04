d9b9f007b603924efbfbeb4ff59139025d6073d5
TalSolodar <talsolodar1@gmail.com>
2022-07-04 20:44:37 +0300

    InputBoardSize : get input from user about the number of the board. and validate the input

--- GetSchwitfy.View/Modules/InputBoardSize.js
+++ GetSchwitfy.View/Modules/InputBoardSize.js
@@ -1,7 +1,7 @@
 class InputBoardSize{
 
     GetBoardSize(){
-        let boardSize = prompt("Enter Board Size:(should be an integer and above 1)");
+        let boardSize = prompt("Please Enter Board Size:(should be an integer and above 1)");
 
         while(!(Number(boardSize) != NaN && boardSize > 1)){
             boardSize = prompt("You wrong value, the value should be an integer and above 1");
