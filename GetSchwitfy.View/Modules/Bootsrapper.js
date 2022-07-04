ef3d734ac17a9b4ff0bda9acb6b1190a8b3f0820
TalSolodar <talsolodar1@gmail.com>
2022-07-04 14:56:40 +0300

    Bootstrapper: initalize the project:

--- GetSchwitfy.View/Modules/Bootsrapper.js
+++ GetSchwitfy.View/Modules/Bootsrapper.js
@@ -1,4 +1,14 @@
+let winnerNotifier = new WinnerNotifier();
+let buttonUpdater = new ButtonUpdater();
+let buttonsEventHandler = new ButtonEventHandler();
 let boardPrinter = new BoardPrinter();
-let gameView = new GameView(boardPrinter);
-let gameSquaresInitializer = new GameSquaresInitializer();
-let gameController = new GameController(gameSquaresInitializer, gameView);
+let gameView = new GameView(boardPrinter, buttonsEventHandler, buttonUpdater, winnerNotifier);
+
+let victoryChecker = new VictoryChecker();
+let gameSolvableChecker = new GameSolvableChecker();
+let squareMovementHandler = new SquareMovementHandler(2,2);
+let gameSquaresInitializer = new GameSquaresInitializer(gameSolvableChecker);
+let gameController = new GameController(gameSquaresInitializer,
+                                        gameView,
+                                        squareMovementHandler,
+                                        victoryChecker);
