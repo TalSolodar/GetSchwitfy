6f077785a456a241537638ef86c333a7ee2044ff
TalSolodar <talsolodar1@gmail.com>
2022-07-04 14:33:40 +0300

    GameContorller: run event handler when button is pressed

--- GetSchwitfy.Controller/GameController.js
+++ GetSchwitfy.Controller/GameController.js
@@ -1,13 +1,26 @@
 class GameController{
     
-    constructor(gameSquaresInitializer, gameView){
+    constructor(gameSquaresInitializer, gameView, squareMovementHandler, victoryChecker){
         this.gameView = gameView;
+        this.squareMovementHandler = squareMovementHandler;
         this.gameSquaresInitializer = gameSquaresInitializer;
+        this.victoryChecker = victoryChecker;
         this.CreateGameBoard();
-        this.gameView.boardPrinter.PrintSquares(this.gameBoard.squares);
     }
 
     CreateGameBoard(){
         this.gameBoard = this.gameSquaresInitializer.InitializeGameBoard();
+        this.gameView.boardPrinter.PrintSquares(this.gameBoard.squares);
+        let buttons = this.gameView.boardPrinter.buttons;
+        this.gameView.buttonsEventHandler.AddButtonsEventListener(buttons, this);
+    }
+
+    HandleButtonEvent(numberButton, buttons){
+        this.squareMovementHandler.gameBoard = this.gameBoard.squares;
+        this.squareMovementHandler.TryMoveSquare(numberButton);
+        this.gameView.buttonUpdater.UpdateButtonsValues(squareMovementHandler.gameBoard, buttons);
+        if(this.victoryChecker.IsBoardSolved(this.gameBoard.squares)){
+            this.gameView.winnerNotifier.NotifyVictory();
+        }
     }
 }
