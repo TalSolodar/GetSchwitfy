a25266a3fcf5c35174e1651d2bdbdf8e97267507
TalSolodar <talsolodar1@gmail.com>
2022-07-04 16:24:17 +0300

    InputBoardSize: receive an input from player which point to the amount of squares in the game

---
+++ GetSchwitfy.View/Modules/InputBoardSize.js
@@ -0,0 +1,12 @@
+class InputBoardSize{
+
+    GetBoardSize(){
+        let boardSize = prompt("Enter Board Size:(should be an integer and above 1)");
+
+        while(!(Number(boardSize) != NaN && boardSize > 1)){
+            boardSize = prompt("You wrong value, the value should be an integer and above 1");
+        }
+
+        return boardSize;
+    }
+}
